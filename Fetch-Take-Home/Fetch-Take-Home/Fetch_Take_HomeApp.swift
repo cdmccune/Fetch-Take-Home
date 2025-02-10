c9ebef7f7bc85ee192496ad99be01be7ffa736bb
Curt McCune <curtmccune@gmail.com>
2025-02-10 16:05:18 -0500

    Create Fetch_Take_HomeApp.swift

---
+++ Fetch-Take-Home/Fetch-Take-Home/Fetch_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_HomeApp.swift
+//  Fetch-Take-Home
+//
+//  Created by Curt McCune on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipesView()
+        }
+    }
+}
